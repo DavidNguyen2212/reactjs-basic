c04569b6b37cca7c5f70dbeb5bed897065d8bce7
DavidNguyen2212 <annhonguoiyeu@gmail.com>
2024-03-03 14:25:09 +0700

    Router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+// BrowserRouter: nơi chứa tất cả vị trí để route trong app (Các nút như Home, Menu...)
+// Routes: Component này chỉ là container chứa các Route
+// Route: thẻ tự đóng, chứa đuôi của con đường + page chính xác
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { Homepage } from './pages/Homepage';
+import { Menu } from './pages/Menu';
+import { Contact } from './pages/Contact';
+import { Navbar } from './Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Routes>
+          {/* self-closing, path: string with '/any', element: component to switch to */}
+          <Route path='/' element={<Homepage />}/>  
+          <Route path='/menu' element={<Menu />}/>  
+          <Route path='/contact' element={<Contact />}/>  
+          <Route path='*' element={<h1>PAGE NOT FOUND</h1>}/>  
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+
+export const Navbar = () => {
+    // Component Link sẽ tạo ra đường dẫn tuyệt đối
+    // Routes so sánh đường dẫn này với các path trong các lựa chọn và nhảy đến pageComponent tương ứng
+    return (
+        <div>
+            <Link to="/">Home </Link>
+            <Link to="/menu"> Menu </Link>
+            <Link to="/contact"> Contact </Link>
+        </div>
+    )
+}
